223bb42d826485d85f354ec7df3f73c322466cd6
quentindurpoix <durpoixquentin@gmail.com>
2017-02-01 11:44:24 +0100

    1TP Fini

--- app/src/main/java/com/durpoix/quentin/retrocollection/AddGame.java
+++ app/src/main/java/com/durpoix/quentin/retrocollection/AddGame.java
@@ -1,13 +1,51 @@
 package com.durpoix.quentin.retrocollection;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddGame extends AppCompatActivity {
-
+    EditText champ;
+    int ChangeGamePosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
+        champ = (EditText)findViewById(R.id.editTextAddNameGame);
+
+        Intent AddGame = getIntent();
+        if(AddGame.hasExtra("ChangeGamePosition")) {
+            ChangeGamePosition = AddGame.getIntExtra("ChangeGamePosition", -1);
+        }
+        if(AddGame.hasExtra("ChangeGame")) {
+
+            String ChangeGameValue = AddGame.getStringExtra("ChangeGame");
+
+
+            if (!ChangeGameValue.equals("")) {
+                champ.setText(ChangeGameValue);
+            }
+        }
+
     }
+
+
+
+
+    public void AddGame(View view) {
+        Intent retour = new Intent();
+        if(ChangeGamePosition!=-1){
+            retour.putExtra("ChangeGamePositon",ChangeGamePosition);
+        }
+        retour.putExtra("NameGame",champ.getText().toString());
+        setResult(Activity.RESULT_OK, retour);
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/durpoix/quentin/retrocollection/Games.java
+++ app/src/main/java/com/durpoix/quentin/retrocollection/Games.java
@@ -1,13 +1,21 @@
 package com.durpoix.quentin.retrocollection;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Games extends AppCompatActivity {
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +25,62 @@ public class Games extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        listview = (ListView) findViewById(R.id.listView);
+
+       listview.setOnItemClickListener(
+               new AdapterView.OnItemClickListener() {
+                   @Override
+                   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                       String ChangeGame=(String)parent.getItemAtPosition(position);
+
+                       Intent retour = new Intent(view.getContext(),AddGame.class);
+                       retour.putExtra("ChangeGame",ChangeGame);
+                       retour.putExtra("ChangeGamePosition",position);
+                       setResult(Activity.RESULT_OK, retour);
+                       startActivityForResult(retour,91);
+
+                   }
+               }
+       );
+
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "D'ici peu on ajoutera un jeu", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                //here
+                startActivityForResult(new Intent(v.getContext(),AddGame.class),90);
             }
         });
+
+
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case 90:
+                if (resultCode == RESULT_OK) {
+                    String res = data.getStringExtra("NameGame");
+
+                    Toast t = Toast.makeText(this, res, Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.CENTER,0,-100); // décalé du centre
+                    t.show();
+                }
+                break;
+            case 91:
+                if (resultCode == RESULT_OK) {
+                    String res = data.getStringExtra("NameGame");
+                    int resPos = data.getIntExtra("ChangeGamePositon",-1);
+                    String ChangedGame=(String)listview.getItemAtPosition(resPos);
+
+                    Toast t = Toast.makeText(this, ChangedGame+" changed in "+res, Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.CENTER,0,-100); // décalé du centre
+                    t.show();
+                }
+                break;
+        }
     }
 
 }
